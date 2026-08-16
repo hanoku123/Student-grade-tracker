f3534e4a788ce35852da154ba820671867a1f1bd
hanoku123 <giduhanoku123@gmail.com>
2026-08-16 17:27:08 +0530

    Add student marks total feature

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class main{
-    public static void main(String[] args){
+     public static void main(String[] args){
         System.out.println("Enter Student Name: ");
         Scanner scanner = new Scanner(System.in);
         String Name = scanner.nextLine();
@@ -11,6 +11,21 @@ public class main{
 
         System.out.println("Student: "+ Name);
         Integer marks = sub1 + sub2 + sub3;
+
         System.out.println("Total Marks: " + marks);
-    }
+        Integer Average = marks / 3;
+        System.out.println("Average marks: " + Average);
+
+        if(Average >= 90){
+            System.out.println("Grade: A(top)");
+        }else if(Average >=70&&Average<90){
+            System.out.println("Grade: B");
+         }else if(Average >=50&&Average<70){
+            System.out.println("Grade: C");
+        }else if(Average >=35&&Average<50){
+             System.out.println("Grade: D");
+         }else if(Average<35){
+             System.out.println("Grade: E(failed)");
+         }
+     }
         }
