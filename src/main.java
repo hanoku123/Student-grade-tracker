27da28731c763c51846c93d00a18d84c2d1acf50
hanoku123 <giduhanoku123@gmail.com>
2026-08-19 14:07:00 +0530

    Refactor student tracker with methods

--- src/main.java
+++ src/main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class main{
-     public static void main(String[] args){
-         Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Number of student: ");
-         Integer Num = scanner.nextInt();
-         System.out.println("Number of student: " + Num);
+        Integer Num = scanner.nextInt();
+        System.out.println("Number of student: " + Num);
         for(int i=0; i<Num; i++){
             System.out.println("Student " + (i+1) + " details" );
             scanner.nextLine();
             System.out.println("Student " + (i+1) + " Name: ");
-            String Name = scanner.nextLine();
+            String Name = getStudentName(scanner);
             System.out.println("Student "+ (i+1) +" "+ Name);
             System.out.println("Enter student "+ (i+1) +" marks for 3 subjects: ");
             Integer sub1 = scanner.nextInt();
@@ -21,7 +21,7 @@ public class main{
             Integer marks = sub1 + sub2 + sub3;
 
             System.out.println("Total Marks: " + marks);
-            Integer Average = marks / 3;
+            int Average = calculateAverage(marks, 3);
             System.out.println("Average marks: " + Average);
 
             if(Average >= 90){
@@ -38,5 +38,16 @@ public class main{
 
         }
 
-     }
-        }
+    }
+    static String getStudentName(Scanner scanner){
+        String Name = scanner.nextLine();
+        return Name;
+    }
+
+
+    static int calculateAverage(int totalMarks, int totalSubjects) {
+        int Average = totalMarks / totalSubjects;
+        return Average;
+    }
+
+}
